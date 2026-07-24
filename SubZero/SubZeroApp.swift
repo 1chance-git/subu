fc096720d12d85141a11c28802a6413631da27ea
chance <1chance41798@gmail.com>
2026-07-23 20:35:55 -0700

    Initial Commit

---
+++ SubZero/SubZeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubZeroApp.swift
+//  SubZero
+//
+//  Created by Ashley Johnson on 7/23/26.
+//
+
+import SwiftUI
+
+@main
+struct SubZeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
